06b3528aa6a5d74611522358eb7c1f7a250f4bc5
Dalton Rothenberger <dalton098@ymail.com>
2024-01-27 12:39:45 -0500

    express serving angular

--- backend/server.ts
+++ backend/server.ts
@@ -1,15 +1,15 @@
-// src/index.js
-const express = require('express');
-const dotenv = require('dotenv');
+
+import express from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req:any , res:any) => {
-  res.send('Express + TypeScript Server');
-});
+const angularPath = (__dirname.endsWith("dist") ? '/../../' : '/../') + 'frontend/Weather101/dist/weather101/browser/';
+
+app.use('/', express.static(__dirname + angularPath));
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
