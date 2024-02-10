98de88cbf182d784476aef2250f4b648c21a8bb5
Joe <joe102192@gmail.com>
2024-02-10 11:32:38 -0500

    fix test

--- frontend/Weather101/src/app/directives/cesium.directive.spec.ts
+++ frontend/Weather101/src/app/directives/cesium.directive.spec.ts
@@ -4,15 +4,21 @@ import { TestBed } from '@angular/core/testing';
 import { provideHttpClient} from '@angular/common/http';
 import { AppComponent } from '../app.component';
 import { By } from '@angular/platform-browser';
+import { AppModule } from '../app.module';
+
+/**
+ * Create a mock element ref to use for testing
+ */
+export class MockElementRef extends ElementRef {}
 
 describe('CesiumDirective', () => {
   var directive: unknown;
 
   beforeEach(() => {
     var fixture = TestBed.configureTestingModule({
-      // ...
-      imports: [CesiumDirective, AppComponent],
-      providers: [provideHttpClient()]
+      imports: [AppModule],
+      declarations: [CesiumDirective],
+      providers: [provideHttpClient(), {provide: ElementRef, useClass: MockElementRef}]
     }).createComponent(AppComponent);
       
     fixture.detectChanges(); // initial binding
