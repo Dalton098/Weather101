567b9fc398b346ec6eeed0c123f41f5627e31c88
Joe Bartelmo <joebartelmo@gmail.com>
2024-02-10 11:23:35 -0500

    wow

--- frontend/Weather101/src/app/directives/cesium.directive.spec.ts
+++ frontend/Weather101/src/app/directives/cesium.directive.spec.ts
@@ -1,8 +1,27 @@
+import { ElementRef } from '@angular/core';
 import { CesiumDirective } from './cesium.directive';
+import { TestBed } from '@angular/core/testing';
+import { provideHttpClient} from '@angular/common/http';
+import { AppComponent } from '../app.component';
+import { By } from '@angular/platform-browser';
 
 describe('CesiumDirective', () => {
+  var directive: unknown;
+
+  beforeEach(() => {
+    var fixture = TestBed.configureTestingModule({
+      // ...
+      imports: [CesiumDirective, AppComponent],
+      providers: [provideHttpClient()]
+    }).createComponent(AppComponent);
+      
+    fixture.detectChanges(); // initial binding
+
+    // all elements with an attached HighlightDirective
+    directive = fixture.debugElement.queryAll(By.directive(CesiumDirective));
+  });
+
   it('should create an instance', () => {
-    const directive = new CesiumDirective();
     expect(directive).toBeTruthy();
   });
 });
