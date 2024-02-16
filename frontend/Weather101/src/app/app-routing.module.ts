8852fef1e0c95facb14b7c4deaaf2b3a53d2f5f6
Dalton Rothenberger <dalton098@ymail.com>
2024-02-16 10:27:44 -0500

    everything

--- frontend/Weather101/src/app/app-routing.module.ts
+++ frontend/Weather101/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HourlyComponent } from './components/hourly/hourly.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+                        {path: '/', component: AppComponent}, 
+                        {path: 'hourlyDn', component: HourlyComponent} 
+                       ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/Weather101/src/app/app.module.ts
+++ frontend/Weather101/src/app/app.module.ts
@@ -5,18 +5,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CesiumDirective } from './directives/cesium.directive';
 import { HttpClientModule } from '@angular/common/http';
+import { MatMenuModule } from '@angular/material/menu';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { HourlyComponent } from './components/hourly/hourly.component';
+import {MatListModule} from '@angular/material/list';
+import { RouterOutlet } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CesiumDirective
+    CesiumDirective,
+    HourlyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatListModule,
+    RouterOutlet
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
