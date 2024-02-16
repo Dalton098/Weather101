31b27bc62bfba3b642e594fcc376147194c8a934
Joe <joe102192@gmail.com>
2024-02-16 09:13:51 -0500

    saving work

--- frontend/Weather101/src/app/directives/cesium.directive.ts
+++ frontend/Weather101/src/app/directives/cesium.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, OnInit, ElementRef } from '@angular/core';
 import { SceneMode, Viewer } from 'cesium';
+import { WeatherService } from '../services/weather.service';
 
 /**
  * A {@link CesiumDirective} is a directive that will initialize a cesium globe and place it inside the provided html tag
@@ -13,7 +14,8 @@ export class CesiumDirective implements OnInit {
    * Constructor
    * @param el The reference to the html element where the cesium element should be placed in
    */
- constructor(private el: ElementRef) { }
+ constructor(private el: ElementRef,
+            private weatherService: WeatherService) { }
 
  /**
   * Initialize the viewer
@@ -21,6 +23,10 @@ export class CesiumDirective implements OnInit {
  ngOnInit() {
   const viewer = new Viewer(this.el.nativeElement, {
     sceneMode: SceneMode.SCENE2D
+  });
+
+  this.weatherService.latLon12HourWeatherForcast(40, 75).subscribe(forecast => {
+    console.log(forecast);
   });
  }
 
---
+++ frontend/Weather101/src/app/services/weather.service.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing';
+
+import { WeatherService } from './weather.service';
+import { HttpClient } from '@angular/common/http';
+
+describe('WeatherService', () => {
+  let service: WeatherService;
+  let httpClient: HttpClient;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(WeatherService);
+    httpClient = TestBed.inject(HttpClient);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('can get 12 hour data', () => {
+    
+  });
+});
