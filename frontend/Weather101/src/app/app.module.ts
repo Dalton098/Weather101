68187fa2def412ed4643fe7c03f0887d0d1fa8e4
Joe <joe102192@gmail.com>
2024-02-24 13:01:13 -0500

    theming oof

--- frontend/Weather101/src/app/app.module.ts
+++ frontend/Weather101/src/app/app.module.ts
@@ -11,9 +11,11 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { HourlyComponent } from './components/hourly/hourly.component';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -26,6 +28,8 @@ import {MatTableModule} from '@angular/material/table';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
     MatMenuModule,
     MatIconModule,
     MatToolbarModule,
