b91c225df872df90cc5a56084386dee5d6a5c4a6
Joe <joe102192@gmail.com>
2024-02-24 10:33:53 -0500

    weather displaying on cesium

--- frontend/Weather101/src/app/directives/cesium.directive.ts
+++ frontend/Weather101/src/app/directives/cesium.directive.ts
@@ -1,5 +1,11 @@
 import { Directive, OnInit, ElementRef } from '@angular/core';
-import { SceneMode, Viewer } from 'cesium';
+import { ImageryLayer, 
+         ImageryProvider,
+         SceneMode,
+         Viewer,
+         WebMapServiceImageryProvider,
+         defaultValue } from 'cesium';
+import { interval } from 'rxjs';
 
 /**
  * A {@link CesiumDirective} is a directive that will initialize a cesium globe and place it inside the provided html tag
@@ -8,6 +14,11 @@ import { SceneMode, Viewer } from 'cesium';
  selector: '[appCesium]'
 })
 export class CesiumDirective implements OnInit {
+
+  /**
+   * The {@link Viewer}
+   */
+  private viewer: Viewer;
 
   /**
    * Constructor
@@ -19,9 +30,49 @@ export class CesiumDirective implements OnInit {
   * Initialize the viewer
   */
  ngOnInit() {
-  const viewer = new Viewer(this.el.nativeElement, {
+  this.viewer = new Viewer(this.el.nativeElement, {
     sceneMode: SceneMode.SCENE2D
   });
+
+  this.addAdditionalLayerOption(
+    "United States Weather Radar",
+    new WebMapServiceImageryProvider({
+      url:
+        "https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi?",
+      layers: "nexrad-n0r",
+      credit: "Radar data courtesy Iowa Environmental Mesonet",
+      parameters: {
+        transparent: "true",
+        format: "image/png",
+      },
+    })
+  );
  }
 
+ /**
+  * Utility function to add an additional imagery layer to the viewer
+  * @param name The name of the imagery provider
+  * @param imageryProviderPromise The {@link ImageryProvider}
+  * @param alpha value controling the alpha of the layer
+  * @param show 
+  */
+ private async addAdditionalLayerOption(name: string,
+                                        imageryProviderPromise: ImageryProvider,
+                                        alpha? : number,
+                                        show?: boolean) {
+    let imageryLayers = this.viewer.imageryLayers;
+    try {
+      const imageryProvider = await Promise.resolve(
+        imageryProviderPromise
+      );
+      const layer = new ImageryLayer(imageryProvider);
+      layer.alpha = defaultValue(alpha, 0.5);
+      layer.show = defaultValue(show, true);
+      imageryLayers.add(layer);
+    } catch (error) {
+      console.error(
+        `There was an error while creating ${name}. ${error}`
+      );
+    }
+  }
 }
