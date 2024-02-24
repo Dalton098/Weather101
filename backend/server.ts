34b4fe7a17c7579008a4c4669585ef59ba081640
Joe <joe102192@gmail.com>
2024-02-24 10:45:24 -0500

    reverse proxy

--- backend/server.ts
+++ backend/server.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT;
 const angularPath = (__dirname.endsWith("dist") ? '/../../' : '/../') + 'frontend/Weather101/dist/weather101/browser/';
 
 app.use('/weather', proxy('https://api.weather.gov'));
-app.use('/nasa', proxy('https://gibs.earthdata.nasa.gov'));
+app.use('/mesonet', proxy('https://mesonet.agron.iastate.edu'));
 
 app.use(express.static(__dirname + angularPath));
 app.get('*', (req, res) => {
