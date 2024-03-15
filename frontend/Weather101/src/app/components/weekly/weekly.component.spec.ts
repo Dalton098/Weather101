21e8b4cecbab1c6e33dc6124fe7f7849c01fd9ab
Joe <joe102192@gmail.com>
2024-03-15 11:09:09 -0400

    test + weekly page done

--- frontend/Weather101/src/app/components/weekly/weekly.component.spec.ts
+++ frontend/Weather101/src/app/components/weekly/weekly.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import * as theSauce from "./testData.json";
 import { WeeklyComponent } from './weekly.component';
+import { of } from 'rxjs';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { WeatherService } from '../../services/weather.service';
 
 describe('WeeklyComponent', () => {
+
+  /**
+   * The weekly component
+   */
   let component: WeeklyComponent;
   let fixture: ComponentFixture<WeeklyComponent>;
 
   beforeEach(async () => {
+    let weatherServiceSpy = jasmine.createSpyObj("WeatherService", ["latLon12HourWeatherForcast"]);
+    weatherServiceSpy.latLon12HourWeatherForcast.and.returnValue(of(theSauce));
     await TestBed.configureTestingModule({
-      declarations: [WeeklyComponent]
+      declarations: [WeeklyComponent],
+      imports: [MatTableModule, MatCardModule],
+      providers: [{provide: WeatherService, useValue: weatherServiceSpy}]
     })
     .compileComponents();
     
@@ -17,7 +30,12 @@ describe('WeeklyComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', async () => {
     expect(component).toBeTruthy();
+    await setTimeout(() => {}, 5000);
+    expect(component.dataSource).toBeDefined();
+    expect(component.todaysDate).toBeDefined();
+    expect(component.todaysForecast).toBeDefined();
+    expect(component.displayedColumns).toBeDefined();
   });
 });
