fd65337119449f29c2ed80337775f16e3bd057b1
Joe Bartelmo <joebartelmo@gmail.com>
2024-03-15 11:15:51 -0400

    oof

--- frontend/Weather101/src/app/components/monthly/monthly.component.ts
+++ frontend/Weather101/src/app/components/monthly/monthly.component.ts
@@ -3,6 +3,8 @@ import { WeatherService } from '../../services/weather.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { getTranslation } from '../../icon-mapping';
 import { CalendarMonthViewBeforeRenderEvent, CalendarMonthViewDay } from 'angular-calendar';
+import { DailyForecast } from '../../common/Forecast';
+import { DailyComponent } from '../daily/daily.component';
 
 export interface MonthlyForecast {
   number:number;
@@ -43,20 +45,59 @@ export class MonthlyComponent implements AfterViewInit {
   public dayAverage = 0;
   public nightAverage = 0;
   public location = "Malvern, PA";
+  private _monthlyForecast = {} as any;
 
   constructor (private weatherService: WeatherService, private changeDetectorRef: ChangeDetectorRef)
   {
   }
 
   ngAfterViewInit(): void {
-    //jank engage
-    var cells = Array.from(document.querySelectorAll('.cal-in-month'));
-    for (let cell of cells) {
-      let day = parseInt((cell as any).innerText);
-      let html = cell.querySelector('span')?.innerHTML as string;
-      html = html + '<div class="calendar-icon">Test Icon</div>';
-      // rewrite html
-      (cell as any).querySelector('span').innerHTML = html;
-    }
+
+    this.weatherService.latLon12HourWeatherForcast(40, 75).subscribe(forecast => 
+      {
+        const days: DailyForecast[] = [];
+        const periods = forecast.properties.periods;
+        for (let i = 0; i < periods.length; i += 2) {
+          days.push(DailyComponent.averageForecast(periods.slice(i, i + 2)));
+        }
+
+        let knownDays = [];
+
+        for (let period of days)
+        {
+          const hourlyForecast:DailyForecast =
+          {
+            humidity: period.relativeHumidity!.value,
+            day: period.name,
+            icon: getTranslation(period.shortForecast),
+            shortForecast: period.shortForecast,
+            startTime: new Date(period.startTime),
+            endTime: new Date(period.endTime),
+            temperature: period.temperature,
+            windDirection: period.windDirection,
+            windSpeed: period.windSpeed
+          }
+          knownDays.push(hourlyForecast.startTime.getDay());
+          this._monthlyForecast[hourlyForecast.startTime.getDay()] = hourlyForecast;
+        }
+
+        // Scramble
+        for (let i = 0; i < 32; i++) {
+          if (!this._monthlyForecast[i]) {
+            const random = Math.floor(Math.random() * knownDays.length);
+            this._monthlyForecast[i] = this._monthlyForecast[knownDays[random]];
+          }
+        }
+
+        //jank engage
+        var cells = Array.from(document.querySelectorAll('.cal-in-month'));
+        for (let cell of cells) {
+          let day = parseInt((cell as any).innerText);
+          let html = cell.querySelector('span')?.innerHTML as string;
+          html = html + `<span class="calendar-icon material-symbols-outlined forecast">${getTranslation(this._monthlyForecast[day].shortForecast)}</span><span class="calendar-text">${this._monthlyForecast[day].temperature}°</span>`;
+          // rewrite html
+          (cell as any).querySelector('span').innerHTML = html;
+        }
+      });
   }
 }
